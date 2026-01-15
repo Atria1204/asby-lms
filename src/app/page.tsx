54c80b198d88fc147e8e0867a28f2528ac8451b2
Atria Caesariano <atriact@gmail.com>
2026-01-15 20:24:32 +0700

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,6 +15,7 @@ export default function Home() {
 
   return (
     <div className="min-h-screen bg-gray-50 font-sans selection:bg-asby-light selection:text-asby-dark flex flex-col">
+        
       <Navbar />
 
       {/* ========================================================= */}
